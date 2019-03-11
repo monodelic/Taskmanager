c682979b2d663e05236a9178af9bdd0efec8736a
Freitag <nepiz@i.ua>
2019-03-11 18:53:15 +0400

    belongs and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ApplicationRecord
   has_secure_password
+<<<<<<< HEAD
+=======
+
+  has_many :my_tasks, class_name: 'Task', foreign_key: :author_id
+  has_many :assigned_tasks, class_name: 'Task', foreign_key: :assignee_id
+
+  validates :first_name, presence: true, length: { minimum: 2 }
+  validates :last_name, presence: true , length: { minimum: 2 }
+  validates :email, presence: true, uniqueness: true, format: { with: /\A\S+@+\S+\z/,
+    message: "wrong email" }
+>>>>>>> belongs and validations
 end
