afecd1b5033b46baea49fd09bb8bac444ac7530f
Freitag <nepiz@i.ua>
2019-03-14 17:27:15 +0400

    coverall test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,25 +37,10 @@ Rails.application.configure do
   config.assets.debug = true
   config.assets.quiet = true
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-end
-config.after_initialize do
-  Bullet.enable = true
-  Bullet.sentry = true
-  Bullet.alert = true
-  Bullet.bullet_logger = true
-  Bullet.console = true
-  Bullet.growl = true
-  Bullet.xmpp = { :account  => 'bullets_account@jabber.org',
-                  :password => 'bullets_password_for_jabber',
-                  :receiver => 'your_account@jabber.org',
-                  :show_online_status => true }
-  Bullet.rails_logger = true
-  Bullet.honeybadger = true
-  Bullet.bugsnag = true
-  Bullet.airbrake = true
-  Bullet.rollbar = true
-  Bullet.add_footer = true
-  Bullet.stacktrace_includes = [ 'your_gem', 'your_middleware' ]
-  Bullet.stacktrace_excludes = [ 'their_gem', 'their_middleware', ['my_file.rb', 'my_method'], ['my_file.rb', 16..20] ]
-  Bullet.slack = { webhook_url: 'http://some.slack.url', channel: '#default', username: 'notifier' }
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.raise = true
+    Bullet.rails_logger = true
+    Bullet.rollbar = true
+  end
 end
