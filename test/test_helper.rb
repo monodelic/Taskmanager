1190bf7202c51e053995ecad2ed05c2382d7a097
Freitag <nepiz@i.ua>
2019-03-14 20:11:37 +0400

    list of users added

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,21 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
+module SignInHelper
+  def sign_in_as(admin)
+    post session_path, params: {
+      session: {
+        password: admin.password,
+        email: admin.email
+      }
+    }
+  end
+end
+
+class ActionDispatch::IntegrationTest
+  include SignInHelper
+end
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   # fixtures :all
