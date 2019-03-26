5c5bc750035613797202b4afe869160d5d9134a5
Freitag <nepiz@i.ua>
2019-03-26 18:34:19 +0400

    resolve conflict

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,12 +5,9 @@ require_relative '../config/environment'
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
-<<<<<<< HEAD
-=======
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   # fixtures :all
   include FactoryBot::Syntax::Methods
 
   # Add more helper methods to be used by all tests here...
->>>>>>> controllers added
 end
